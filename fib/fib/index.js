400f6834aaace6eb57ba67f894203c2ad23bad2e
Pete Ayres <36248449+pkayres@users.noreply.github.com>
2020-04-27 14:48:56 -0400

    Add files via upload

--- fib/fib/index.js
+++ fib/fib/index.js
@@ -10,25 +10,39 @@
 
 // // iterative solution
 //
-function fib(num){
-  const fibArr = [0,1];
-
-  for(let i = 2; i <= num; i++){
-
-    // this always gets you the last element in array
-    const a = fibArr[fibArr.length - 1];
-    // this always gets you the second to last element in array
-    const b = fibArr[fibArr.length - 2];
-
-    // this pushes the new element into array created by adding the last and second to last element (a+B) together.
-    fibArr.push(a + b)
-  }
-  // finally return the last element in the final array once loop completes
-  return fibArr[fibArr.length-1]
-}
-fib(7)
+// function fib(num){
+//   const fibArr = [0,1];
+//
+//   for(let i = 2; i <= num; i++){
+//
+//     // this always gets you the last element in array
+//     const a = fibArr[fibArr.length - 1];
+//     // this always gets you the second to last element in array
+//     const b = fibArr[fibArr.length - 2];
+//
+//     // this pushes the new element into array created by adding the last and second to last element (a+B) together.
+//     fibArr.push(a + b)
+//   }
+//   // finally return the last element in the final array once loop completes
+//   return fibArr[fibArr.length-1]
+// }
+// fib(7)
 
 // recursive solution
+
+function memoize(fn){
+  const cache = {};
+  return function(...args){
+    if (cache[args]){
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+
+    cache[args] = result;
+    return result;
+  }
+}
 
 function fib(n){
 
@@ -38,6 +52,7 @@ function fib(n){
   return fib(n - 1) + fib(n - 2);
 }
 
+fib = memoize(fib);
 
 
 module.exports = fib;
